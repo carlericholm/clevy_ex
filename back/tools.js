a0334fd3d973544007ac2049b3b5bdb8c6e5d641
Carl-eric HOLM <cholm@e2r6p17.42.fr>
2018-12-12 19:18:01 +0100

    clevy / simple chatbot exercice

--- back/tools.js
+++ back/tools.js
@@ -6,51 +6,51 @@ var con = require('../config/database');
 
 const getName = (GET_NAME_API) => {
 	return fetch(GET_NAME_API)
-	.then(res => res.json())
-	.then(json => {
-		var firstname = json.results[0].name.first;
-		var lastname = json.results[0].name.last;
-		var name = firstname + ' ' + lastname;
-		return name;
-	})
-}
+		.then(res => res.json())
+		.then(json => {
+			var firstname = json.results[0].name.first;
+			var lastname = json.results[0].name.last;
+			var name = firstname + ' ' + lastname;
+			return name;
+		});
+};
 
 const addLogDb = (question, answer) => {
 	var sql = "INSERT INTO logs set question = ?, answer = ?";
-	con.query(sql, [question, answer],(err, result) => { if (err) throw err;});
-}
+	con.query(sql, [question, answer],(err) => { if (err) throw err;});
+};
 
 module.exports = {
 	// parses csv file, check matching intent, log the result
 	logResult: (intent, GET_NAME_API, question) => {
 		var array = new Array();
 		var csvStream = csv
-		.parse({delimiter: ';'})
-		.on("data", (data) => {
-			array.push(data);
-		})
-		.on("end", () => {
-			array.forEach((element) => {
-				if (element[0] === intent.slug)
-				{
-					if (intent.slug === 'ask-creator')
-					{
-						getName(GET_NAME_API).then((name) => {
-							var str = element[1].replace('${name}', name);
-							//add question / answer into DB
-							addLogDb(question, str);
-							console.log(" // " + str);
-						})
-					}
-					else
-					{
-						//add question / answer into DB
-						addLogDb(question, element[1]);
-						console.log(" // " + element[1]);
-					}
-				}
+			.parse({delimiter: ';'})
+			.on("data", (data) => {
+				array.push(data);
 			})
-		});
+			.on("end", () => {
+				array.forEach((element) => {
+					if (element[0] === intent.slug)
+					{
+						if (intent.slug === 'ask-creator')
+						{
+							getName(GET_NAME_API).then((name) => {
+								var str = element[1].replace('${name}', name);
+								//add question / answer into DB
+								addLogDb(question, str);
+								console.log(" // " + str);
+							});
+						}
+						else
+						{
+							//add question / answer into DB
+							addLogDb(question, element[1]);
+							console.log(" // " + element[1]);
+						}
+					}
+				});
+			});
 		stream.pipe(csvStream);
 	}
-}
+};
--- config/database.js
+++ config/database.js
@@ -1,18 +1,18 @@
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root42"
+	host: "localhost",
+	user: "root",
+	password: "root42"
 });
 
 con.connect((err) => {
-  if (err) throw err;
-  con.query("CREATE DATABASE IF NOT EXISTS clevy", (err, result) => { if (err) throw err; });
-  con.query('USE `clevy`', (err) => { if (err) throw err });
+	if (err) throw err;
+	con.query("CREATE DATABASE IF NOT EXISTS clevy", (err) => { if (err) throw err; });
+	con.query('USE `clevy`', (err) => { if (err) throw err; });
 
-  var sql = "CREATE TABLE IF NOT EXISTS logs (id INT PRIMARY KEY AUTO_INCREMENT, question VARCHAR(255), answer VARCHAR(255))";
-  con.query(sql, (err, result) => { if (err) throw err;});
+	var sql = "CREATE TABLE IF NOT EXISTS logs (id INT PRIMARY KEY AUTO_INCREMENT, question VARCHAR(255), answer VARCHAR(255))";
+	con.query(sql, (err) => { if (err) throw err;});
   
 });
 
