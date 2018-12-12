f7be5727bc4ec58d5ba9029373861c7fe6b23db3
Carl-eric HOLM <cholm@e2r6p17.42.fr>
2018-12-12 18:53:14 +0100

    clevy / simple chatbot exercice

---
+++ config/database.js
@@ -0,0 +1,19 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root42"
+});
+
+con.connect((err) => {
+  if (err) throw err;
+  con.query("CREATE DATABASE IF NOT EXISTS clevy", (err, result) => { if (err) throw err; });
+  con.query('USE `clevy`', (err) => { if (err) throw err });
+
+  var sql = "CREATE TABLE IF NOT EXISTS logs (id INT PRIMARY KEY AUTO_INCREMENT, question VARCHAR(255), answer VARCHAR(255))";
+  con.query(sql, (err, result) => { if (err) throw err;});
+  
+});
+
+module.exports = con;
---
+++ index.js
@@ -0,0 +1,32 @@
+var con = require('./config/database');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+const recastai = require('recastai').default
+const fetch = require('node-fetch');
+const tools = require('./back/tools');
+
+// Usefull const
+const PORT = 8080;
+const GET_NAME_API = 'https://randomuser.me/api/';
+const RECAST_TOKEN = '901ae589a236643b918f5f6a293adfde'
+// get string passed as first argument
+const ARG = process.argv[2];
+
+const request = new recastai.request(RECAST_TOKEN);
+
+request.analyseText(ARG)
+.then((res) => {
+    // get the intent detected
+    var intent = res.intent()
+    if (intent)
+    	tools.logResult(intent, GET_NAME_API, ARG);
+    else
+    	console.log(" // Sorry, unknown intent");
+})
+.catch((err) => {
+	console.log(err);
+})
+
+app.listen(PORT);
